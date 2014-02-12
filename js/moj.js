a215792274db52ea77ce37c298c1ba9de480b202
kkovac <kovack@gmail.com>
2014-02-12 11:56:06 +0100

    4

--- js/moj.js
+++ js/moj.js
@@ -11,11 +11,16 @@ $(document).ready(function () {
         DajZadjnePartnere();
     });
 
+    PieChart();
+    StackedColumn();
+    //DajZadjnePartnere();
+
+    $("#sectiongadgetdetail").hide();
 
 
     function DajZadjnePartnere() {
         var formatiraniHTML = "";
-
+        var i = 0;
         $.ajax({
             url: "http://www.spin.hr/ng/servicezapredavajne/",
             dataType: 'jsonp',
@@ -23,14 +28,27 @@ $(document).ready(function () {
             timeout: 10000,
             success: function (data, status) {
                 $.each(data, function (i, item) {
-                    formatiraniHTML = formatiraniHTML + '<div class="col-md-4"><div class="mojitem" data-id="' + item.partneriid + '" >' +
-                        item.naziv + '<br/>' +
-                        item.mjestanaziv + '<br/>' +
-                        item.adresa +
-                        '</div></div>';
+
+                    i = i + 1;
+
+                    formatiraniHTML = formatiraniHTML + '<div class="col-md-4"><div class="mojitem clearfix" data-partneriid="' + item.partneriid + '" >'
+                        + '<span class="col-xs-7  ">'
+                        + item.naziv + '<br/>' 
+                        + item.mjestanaziv + '<br/>' 
+                        + item.adresa 
+                        + '</span>'
+                        + '<span class="col-xs-5">'
+                        + '<h4>'
+                        + item.vrijednostspdv
+                        + 'kn</h4>'
+                        + '</span>'
+                        + '</div></div>';
+
+                    if (i == 3) { formatiraniHTML = formatiraniHTML + '<div class="clearfix"></div>'; i = 0; }
+
                 });
                 $("#lista").html(formatiraniHTML);
-                $(".mojitem").on('click', function () { PartneriDet($(this).attr("data-id")); });
+                $(".mojitem").on('click', function () { PartneriDet($(this).attr("data-partneriid")); });
             },
             error: function (xhr, ajaxOptions, thrownError) {
                 alert(thrownError);
@@ -43,6 +61,136 @@ $(document).ready(function () {
     function PartneriDet(id) {
         alert(id);
     }
+
+
+
+    function PieChart() {
+
+        // Radialize the colors
+        Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function (color) {
+            return {
+                radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+                stops: [
+		            [0, color],
+		            [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+                ]
+            };
+        });
+
+        // Build the chart
+        $('#container1').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: 'Browser market shares at a specific website, 2010'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        color: '#000000',
+                        connectorColor: '#000000',
+                        formatter: function () {
+                            return '<b>' + this.point.name + '</b>: ' + this.percentage + ' %';
+                        }
+                    }
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Browser share',
+                data: [
+                    ['Firefox', 45.0],
+                    ['IE', 26.8],
+                    {
+                        name: 'Chrome',
+                        y: 12.8,
+                        sliced: true,
+                        selected: true
+                    },
+                    ['Safari', 8.5],
+                    ['Opera', 6.2],
+                    ['Others', 0.7]
+                ]
+            }]
+        });
+    }
+
+
+    
+
+    function StackedColumn() {
+        $('#container2').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Stacked column chart'
+            },
+            xAxis: {
+                categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Total fruit consumption'
+                },
+                stackLabels: {
+                    enabled: true,
+                    style: {
+                        fontWeight: 'bold',
+                        color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                    }
+                }
+            },
+            legend: {
+                align: 'right',
+                x: -70,
+                verticalAlign: 'top',
+                y: 20,
+                floating: true,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColorSolid) || 'white',
+                borderColor: '#CCC',
+                borderWidth: 1,
+                shadow: false
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.x + '</b><br/>' +
+                        this.series.name + ': ' + this.y + '<br/>' +
+                        'Total: ' + this.point.stackTotal;
+                }
+            },
+            plotOptions: {
+                column: {
+                    stacking: 'normal',
+                    dataLabels: {
+                        enabled: true,
+                        color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+                    }
+                }
+            },
+            series: [{
+                name: 'John',
+                data: [5, 3, 4, 7, 2]
+            }, {
+                name: 'Jane',
+                data: [2, 2, 3, 2, 1]
+            }, {
+                name: 'Joe',
+                data: [3, 4, 4, 2, 5]
+            }]
+        });
+    }
+
 
 
 
